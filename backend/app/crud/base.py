b699b1b3bdcefc2a4dc5437d15f523ed96fff182
chakhva <aleksandre.chakhvashvili24@gmail.com>
2025-06-07 04:56:34 +0400

    change to python 3.10 style

--- backend/app/crud/base.py
+++ backend/app/crud/base.py
@@ -1,25 +1,29 @@
+from typing import Any, Generic, TypeVar
+
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.core.database import Base
 
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
 
-class CRUDBase[
-    ModelType: Base,
-    CreateSchemaType: BaseModel,
-    UpdateSchemaType: BaseModel,
-]:
-    def init(self, model: type[ModelType]):
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: type[ModelType]):
         """
         CRUD object with default methods to Create, Read, Update, Delete (CRUD).
-        Parameters
-        * model: A SQLAlchemy model class
-        * schema: A Pydantic model (schema) class
+
+        **Parameters**
+
+        * `model`: A SQLAlchemy model class
+        * `schema`: A Pydantic model (schema) class
         """
         self.model = model
 
-    def get(self, db: Session, id: any) -> ModelType | None:
+    def get(self, db: Session, id: Any) -> ModelType | None:
         return db.query(self.model).filter(self.model.id == id).first()
 
     def get_multi(
