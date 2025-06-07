d5cae5f09585071e2eeab84e92fde3d81c4d709b
Gafro <nika54444@gmail.com>
2025-06-08 03:01:02 +0400

    fixed init

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,15 +1,15 @@
 from app.crud.notification_log import CRUDNotificationLog
 from app.crud.reminder import CRUDReminder
 from app.crud.subscription import CRUDSubscription
+from app.crud.subscription_tag import CRUDSubscriptionTag
 from app.crud.tag import CRUDTag
 from app.crud.user import CRUDUser
-from app.crud.subscription_tag import CRUDSubscriptionTag
 
 __all__ = [
     "CRUDNotificationLog",
     "CRUDReminder",
     "CRUDSubscription",
+    "CRUDSubscriptionTag",
     "CRUDTag",
     "CRUDUser",
-    "CRUDSubscriptionTag",
 ]
