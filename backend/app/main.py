3d103618862954dd40f4cdeea29d646673b8dbb9
chakhva <aleksandre.chakhvashvili24@gmail.com>
2025-06-08 02:05:46 +0400

    fix formatting issues

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
 app = FastAPI(
-    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    title=settings.APP_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
 # Set up CORS
