52bc17ded78d6838076c86e0eed43b60ac833b38
chakhva <aleksandre.chakhvashvili24@gmail.com>
2025-06-07 04:06:08 +0400

    update main test

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -8,12 +8,10 @@ client = TestClient(fastapi_app)
 def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
-    assert response.json() == {"status": "ok", "db_host": None}
+    assert response.json() == {"status": "healthy"}
 
 
 def test_root_path():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {
-        "message": "Welcome to Subscope API!"
-    }  # Adjust expected message
+    assert response.json() == {"message": "Welcome to the FastAPI application!"}
